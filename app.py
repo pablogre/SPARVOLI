2d90c86b59ff2a3d95149597049c22f69b6da673
pablogre <38010408+pablogre@users.noreply.github.com>
2025-06-05 11:17:12 -0300

    subiendo a yRAILWAY

--- app.py
+++ app.py
@@ -362,4 +362,6 @@ def cancelar():
 
 
 if __name__ == "__main__":
-     app.run(debug=True, host='0.0.0.0', port=5002)
+     port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    #app.run(debug=True, host='0.0.0.0', port=5002)
